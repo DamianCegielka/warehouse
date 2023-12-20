a2967ea32c7ef64e5014c6195b30e5358a30a78c
Damian Cegiełka <cegielka.damian@gmail.com>
2023-12-20 12:59:53 +0100

    Refactor DownloadExchangeRateFromURL class

--- src/main/java/org/project/repository/load/DownloadExchangeRateFromURL.java
+++ src/main/java/org/project/repository/load/DownloadExchangeRateFromURL.java
@@ -1,6 +1,7 @@
 package org.project.repository.load;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,88 +13,46 @@ public class DownloadExchangeRateFromURL {
     String[] SplitRate;
     String Date;
     Double Rate;
-    public Map<String,Double> tabelaNBP=new TreeMap();
+    public Map<String, Double> tabelaNBP = new TreeMap();
 
     public void rate() {
         try {
+            String tableFromNBP2020="https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2020.csv";
+            String tableFromNBP2021 = "https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2021.csv";
+            String tableFromNBP2022 = "https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2022.csv";
+            String tableFromNBP2023 = "https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2023.csv";
 
-            URL url = new URL("https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2021.csv");
-            URLConnection urlCon = url.openConnection();
-            BufferedReader read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            this.downloadDataFromNBP(tableFromNBP2020);
+            this.downloadDataFromNBP(tableFromNBP2021);
+            this.downloadDataFromNBP(tableFromNBP2022);
+            this.downloadDataFromNBP(tableFromNBP2023);
 
-            String tmp;
-
-            read.readLine();
-            read.readLine();
-
-            while ((tmp=read.readLine()) != null) {
-                SplitRate= tmp.split(";");
-                Date=SplitRate[0];
-                if(Date.trim().length()!=8) break;
-                Rate=Double.parseDouble(SplitRate[2].replaceAll(",", "."));
-                tabelaNBP.put(Date,Rate);
-                System.out.println(Date+" "+Rate);
-
-            }
-
-            url = new URL("https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2022.csv");
-            urlCon = url.openConnection();
-            read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
-
-            read.readLine();
-            read.readLine();
-
-            while ((tmp=read.readLine()) != null) {
-                SplitRate= tmp.split(";");
-                Date=SplitRate[0];
-                if(Date.trim().length()!=8) break;
-                Rate=Double.parseDouble(SplitRate[2].replaceAll(",", "."));
-                tabelaNBP.put(Date,Rate);
-                System.out.println(Date+" "+Rate);
-
-            }
-
-            url = new URL("https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2023.csv");
-            urlCon = url.openConnection();
-            read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
-
-            read.readLine();
-            read.readLine();
-
-            while ((tmp=read.readLine()) != null) {
-                SplitRate= tmp.split(";");
-                Date=SplitRate[0];
-                if(Date.trim().length()!=8) break;
-                Rate=Double.parseDouble(SplitRate[2].replaceAll(",", "."));
-                tabelaNBP.put(Date,Rate);
-                System.out.println(Date+" "+Rate);
-
-            }
-
-            url = new URL("https://www.nbp.pl/kursy/Archiwum/archiwum_tab_a_2020.csv");
-            urlCon = url.openConnection();
-            read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
-
-            read.readLine();
-            read.readLine();
-
-            while ((tmp=read.readLine()) != null) {
-                SplitRate= tmp.split(";");
-                Date=SplitRate[0];
-                if(Date.trim().length()!=8) break;
-                Rate=Double.parseDouble(SplitRate[2].replaceAll(",", "."));
-                tabelaNBP.put(Date,Rate);
-                System.out.println(Date+" "+Rate);
-
-            }
-
-            System.out.println("Pobieranych danych serwerowych zakoñczone powodzeniem");
-
-            read.close();
-
+            System.out.println("Pobieranych danych serwerowych zakonczone powodzeniem");
         } catch (Exception e) {
+            System.out.println("Pobieranych danych zakonczone bledem");
             e.printStackTrace();
         }
+    }
+
+    private void downloadDataFromNBP(String link) throws IOException {
+        URL url = new URL(link);
+        URLConnection urlCon = url.openConnection();
+        BufferedReader read = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+
+        String tmp;
+
+        read.readLine();
+        read.readLine();
+
+        while ((tmp = read.readLine()) != null) {
+            SplitRate = tmp.split(";");
+            Date = SplitRate[0];
+            if (Date.trim().length() != 8) break;
+            Rate = Double.parseDouble(SplitRate[2].replaceAll(",", "."));
+            tabelaNBP.put(Date, Rate);
+            System.out.println(Date + " " + Rate);
+        }
+        read.close();
     }
 }
 
